334a9ddfe5f9d1dbd6b6af8284e85388e292022a
Aditi3 <Aditi3@users.noreply.github.com>
2022-07-04 15:24:51 +0200

    chore: create AdjustSdk target

---
+++ Adjust/AdjustSdk/AdjustSdk.h
@@ -0,0 +1,19 @@
+//
+//  AdjustSdk.h
+//  AdjustSdk
+//
+//  Created by Aditi Agrawal on 04/07/22.
+//  Copyright © 2022 Adjust GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdjustSdk.
+FOUNDATION_EXPORT double AdjustSdkVersionNumber;
+
+//! Project version string for AdjustSdk.
+FOUNDATION_EXPORT const unsigned char AdjustSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjustSdk/PublicHeader.h>
+
+
