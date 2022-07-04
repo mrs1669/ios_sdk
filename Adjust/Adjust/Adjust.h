3284574715fade66ec4df9c25b627bc1ad9965cb
Aditi3 <Aditi3@users.noreply.github.com>
2022-07-04 14:38:51 +0200

    chore: create Adjust.xcodeproj

---
+++ Adjust/Adjust/Adjust.h
@@ -0,0 +1,12 @@
+//
+//  Adjust.h
+//  Adjust
+//
+//  Created by Aditi Agrawal on 04/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Adjust : NSObject
+
+@end
---
+++ Adjust/Adjust/Adjust.m
@@ -0,0 +1,12 @@
+//
+//  Adjust.m
+//  Adjust
+//
+//  Created by Aditi Agrawal on 04/07/22.
+//
+
+#import "Adjust.h"
+
+@implementation Adjust
+
+@end
