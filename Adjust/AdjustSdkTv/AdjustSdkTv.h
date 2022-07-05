1e562cbbd3f01d9714898097a9827809db9d4575
Aditi3 <Aditi3@users.noreply.github.com>
2022-07-05 13:03:23 +0200

    chore: create AdjustSdkTv target

---
+++ Adjust/AdjustSdkTv/AdjustSdkTv.h
@@ -0,0 +1,19 @@
+//
+//  AdjustSdkTv.h
+//  AdjustSdkTv
+//
+//  Created by Aditi Agrawal on 05/07/22.
+//  Copyright © 2022 Adjust GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdjustSdkTv.
+FOUNDATION_EXPORT double AdjustSdkTvVersionNumber;
+
+//! Project version string for AdjustSdkTv.
+FOUNDATION_EXPORT const unsigned char AdjustSdkTvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjustSdkTv/PublicHeader.h>
+
+
