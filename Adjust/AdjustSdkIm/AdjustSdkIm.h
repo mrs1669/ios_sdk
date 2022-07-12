885d30648dd410f24cffb1364e6db27b7a394b3f
Aditi3 <Aditi3@users.noreply.github.com>
2022-07-12 10:44:02 +0200

    chore: create `AdjustSdkIm` target

---
+++ Adjust/AdjustSdkIm/AdjustSdkIm.h
@@ -0,0 +1,19 @@
+//
+//  AdjustSdkIm.h
+//  AdjustSdkIm
+//
+//  Created by Aditi Agrawal on 05/07/22.
+//  Copyright © 2022 Adjust GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdjustSdkIm.
+FOUNDATION_EXPORT double AdjustSdkImVersionNumber;
+
+//! Project version string for AdjustSdkIm.
+FOUNDATION_EXPORT const unsigned char AdjustSdkImVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjustSdkIm/PublicHeader.h>
+
+
