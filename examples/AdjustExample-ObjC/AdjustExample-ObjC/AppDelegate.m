817c41726749367ca365cc1083b43a7e374dbf9c
Aditi3 <Aditi3@users.noreply.github.com>
2022-08-04 10:54:20 +0200

    task: update example project
    Add track event api support

--- examples/AdjustExample-ObjC/AdjustExample-ObjC/AppDelegate.m
+++ examples/AdjustExample-ObjC/AdjustExample-ObjC/AppDelegate.m
@@ -8,6 +8,7 @@
 #import "AppDelegate.h"
 
 #import <Adjust/ADJAdjust.h>
+#import <Adjust/ADJAdjustEvent.h>
 
 @interface AppDelegate ()
 
@@ -19,10 +20,14 @@
     // Override point for customization after application launch.
     
     ADJAdjustConfig *_Nonnull adjustConfig =
-    [[ADJAdjustConfig alloc] initWithAppToken:@"abc"
+    [[ADJAdjustConfig alloc] initWithAppToken:@"2fm9gkqubvpc"
                                   environment:ADJEnvironmentSandbox];
     
     [ADJAdjust sdkInitWithAdjustConfig:adjustConfig];
+    
+    ADJAdjustEvent *event = [[ADJAdjustEvent alloc]
+                             initWithEventId:@"g3mfiw"];
+    [ADJAdjust trackEvent:event];
     
     return YES;
 }
