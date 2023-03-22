ca154e8645ca0da4ebf31fff9d8d756f64f2eff0
Aditi3 <Aditi3@users.noreply.github.com>
2023-03-22 10:42:10 +0100

    test: add external device id directory

--- tests/AdjustTestApp/AdjustTestApp/ViewController.m
+++ tests/AdjustTestApp/AdjustTestApp/ViewController.m
@@ -70,6 +70,7 @@ static NSString * controlUrl = @"ws://127.0.0.1:1987";
     [self.testLibrary addTestDirectory:@"session-tracking"];
     [self.testLibrary addTestDirectory:@"stop-restart"];
     [self.testLibrary addTestDirectory:@"third-party-sharing"];
+    [self.testLibrary addTestDirectory:@"external-device-id"];
 
 }
 
